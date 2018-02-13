07c8627dcb747683e3bf93508d04ce4d5c471696
jmanfredi <juanjmanfredi@gmail.com>
2018-02-13 11:52:12 -0500

    Organized utilities

--- insight_testsuite/temp/src/donation-analytics.py
+++ insight_testsuite/temp/src/donation-analytics.py
@@ -2,9 +2,11 @@
 import os
 import sys
 import datetime
+from contextlib import contextmanager
 
 import numpy as np
 
+from dautilities import smart_open,calc_percentile
 
 #Define fixed indices for columns of interest
 i_recID = 0
@@ -18,112 +20,94 @@ numCols = 21
 
 def main(argv):
     
-#    percFileName = 'insight_testsuite/tests/test_1/input/percentile.txt'
-#    percFileName = 'insight_testsuite/tests/percTest/input/percentile.txt'
-#    percFileName = 'insight_testsuite/tests/recordYearTest/input/percentile.txt'
-    percFileName = 'input/percentile.txt'
+    percFileName = argv[1]
+    inFileName = argv[2]
+    outFileName = argv[3]
+    
     perc = -1
-    with open(percFileName) as percFile:
-        perc = float(percFile.readline().strip())
+    with smart_open(percFileName) as (percFile,err):
+        if err:
+            raise IOError('Unable to open percent input file: '+percFileName+'\n')
+        else:
+            perc = float(percFile.readline().strip())
 
 
     if(perc <=0 or perc > 100):
         raise ValueError('Percent input invalid! Must be > 0 and <= 100\n')
         
-    print('Calculate percentile at ' + str(perc) + ' percent\n')
-    
-    #Control inputs
-    
     donors = {}
     repeats = {}
-
-    #TEMPORARY
-    #    inFileName = 'insight_testsuite/tests/test_1/input/itcont.txt'
-    inFileName = 'input/itcont.txt'
-#    inFileName = 'insight_testsuite/tests/percTest/input/itcont.txt'
-#    inFileName = 'insight_testsuite/tests/recordYearTest/input/itcont.txt'
-    #    outFileName = 'insight_testsuite/tests/test_1/output/repeat_donors.txt'
-    outFileName = 'output/repeat_donors.txt'
-#    outFileName = 'insight_testsuite/tests/percTest/output/repeat_donors.txt'
- #   outFileName = 'insight_testsuite/tests/recordYearTest/output/repeat_donors.txt'
-
-    #I/O??? STREAMING?
-    with open(inFileName,'r') as inFile, open(outFileName,'w') as outFile:
-
-        recordCount = 0
-
-        for line in inFile:
-#            print(line)
-            recordCount+=1
-
-            inList = line.strip().split('|')
-            
-            if (len(inList) != numCols):
-                sys.stderr.write('Line '+str(recordCount)+' has ' + str(len(inList)) + ' columns, not ' + str(numCols)+'. Skipping.\n')
-                continue
-            
-            #The condition below does not correspond to an error in the data, so we silently skip this line.
-            if (inList[i_other]):
-                continue
-
-            recID = inList[i_recID]
-            if (recID==''):
-                sys.stderr.write('Line '+str(recordCount)+' has an empty recipientID. Skipping.\n')
-            
-            donorName = inList[i_name]
-            if (donorName==''):
-                sys.stderr.write('Line '+str(recordCount)+' has an empty name. Skipping.\n')
-                
-            zipCode = inList[i_zipCode][0:5]
-            if (len(zipCode) < 5):
-                sys.stderr.write('Line '+str(recordCount)+' has a bad zipcode: '+zipCode+'. Skipping.\n')
-                continue
-            
-            try:
-                tDate = datetime.datetime.strptime(inList[i_tDate],'%m%d%Y')
-            except ValueError:
-                sys.stderr.write('Line ' +str(recordCount)+' has bad datetime. Skipping.\n')
-                continue
-
-            try:
-                tAmount = int(round(float(inList[i_tAmount])))
-            except ValueError:
-                sys.stderr.write('Line '+str(recordCount)+' has a bad transaction amount. Skipping.\n')
-            
-            donorKey = donorName + zipCode
- #           print(donorKey)
-
-            if donorKey not in donors:
-                firstTyr = tDate.year
-                donors[donorKey] = firstTyr
-            elif tDate.year < donors[donorKey]:
-                #Encountered a transaction from this donor that came before the "first" one: skip this.
-                continue
-            else:
-                repeatKey = recID + str(tDate.year) + zipCode
-#                print(repeatKey)
-
-                if repeatKey not in repeats:
-                    repeats[repeatKey] = []
-
-                repeats[repeatKey].append(tAmount)
-                percVal = calc_percentile(repeats[repeatKey],perc)
-
-                tNum = len(repeats[repeatKey])
-                tSum = sum(repeats[repeatKey])
-            
-                outputStr= (recID + '|' + zipCode + '|' + str(tDate.year) + '|' + str(percVal) +
-                          '|' + str(tSum) + '|' + str(tNum)) + '\n'
-
-                print(outputStr.strip())
-                outFile.write(outputStr)
-
     
+    with smart_open(inFileName,'r') as (inFile,inErr), smart_open(outFileName,'w') as (outFile,outErr):
+        if inErr:
+            raise IOError('Unable to open input file: '+inFileName+'\n')
+        elif outErr:
+            raise IOError('Unable to open output file: '+outFileName+'\n')
+        else:
+            recordCount = 0
+            
+            for line in inFile:
+                recordCount+=1
+                
+                inList = line.strip().split('|')
+                
+                if (len(inList) != numCols):
+                    sys.stderr.write('Line '+str(recordCount)+' has ' + str(len(inList)) + ' columns, not ' + str(numCols)+'. Skipping.\n')
+                    continue
+            
+                #The condition below does not correspond to an error in the data, so we silently skip this line.
+                if (inList[i_other]):
+                    continue
 
-def calc_percentile(values,percent):
-    '''Given a list of values and a percent value, calculate the percentile using the nearest-rank method'''
-    index = int(np.ceil((float(percent)/100)*len(values)))
-    return sorted(values)[index-1]
+                recID = inList[i_recID]
+                if (recID==''):
+                    sys.stderr.write('Line '+str(recordCount)+' has an empty recipientID. Skipping.\n')
+            
+                donorName = inList[i_name]
+                if (donorName==''):
+                    sys.stderr.write('Line '+str(recordCount)+' has an empty name. Skipping.\n')
+                
+                zipCode = inList[i_zipCode][0:5]
+                if (len(zipCode) < 5):
+                    sys.stderr.write('Line '+str(recordCount)+' has a bad zipcode: '+zipCode+'. Skipping.\n')
+                    continue
+            
+                try:
+                    tDate = datetime.datetime.strptime(inList[i_tDate],'%m%d%Y')
+                except ValueError:
+                    sys.stderr.write('Line ' +str(recordCount)+' has bad datetime. Skipping.\n')
+                    continue
+
+                try:
+                    tAmount = int(round(float(inList[i_tAmount])))
+                except ValueError:
+                    sys.stderr.write('Line '+str(recordCount)+' has a bad transaction amount. Skipping.\n')
+            
+
+                donorKey = donorName + zipCode
+
+                if donorKey not in donors:
+                    firstTyr = tDate.year
+                    donors[donorKey] = firstTyr
+                elif tDate.year < donors[donorKey]:
+                    #Encountered a transaction from this donor that came before the "first" one: skip this.
+                    continue
+                else:
+                    repeatKey = recID + str(tDate.year) + zipCode
+                    if repeatKey not in repeats:
+                        repeats[repeatKey] = []
+
+                    repeats[repeatKey].append(tAmount)
+                    percVal = calc_percentile(repeats[repeatKey],perc)
+
+                    tNum = len(repeats[repeatKey])
+                    tSum = sum(repeats[repeatKey])
+            
+                    outputStr= (recID + '|' + zipCode + '|' + str(tDate.year) + '|' + str(percVal) +
+                                '|' + str(tSum) + '|' + str(tNum)) + '\n'
+
+                    outFile.write(outputStr)
+
 
     
 if __name__ == "__main__":
